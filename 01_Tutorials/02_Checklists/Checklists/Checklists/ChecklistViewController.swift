c98aa4bfe63da7b35331065e3dec310271e56a95
Sam Kyu, Kang <sky4411v@gmail.com>
2018-02-02 18:45:56 +0900

    0202 tablevie didselect control

--- 01_Tutorials/02_Checklists/Checklists/Checklists/ChecklistViewController.swift
+++ 01_Tutorials/02_Checklists/Checklists/Checklists/ChecklistViewController.swift
@@ -30,9 +30,27 @@ class ChecklistViewController: UITableViewController{
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 100
     }
+    
+    //셀이 선택되었을때, 작동하는 메서드
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //체크마크의 온/오프 기능을 넣는 코드를 구현한다.
+        //하지만 셀 재사용으로 인해, 해당 코드는 재사용 되면서, 의도 되지 않은 곳에 체크박스가 걸려있는 경우가 있다.
+        if let cell = tableView.cellForRow(at: indexPath) {
+            //엑세서리가 활성화 되있지 않은 경우 활성화를 시켜주고 그렇지 않은 경우 비활성화를 시켜주는 코드를 구현한다.
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        // 하이라이트 된 회색 음영을 없애는 코드를 작성하여 준다.
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
         
         //label에 태그를 붙여서 해당 태그에 해당하는 레이블의 텍스트 값을 주기위한 코드를 작성해보자.
         let label = cell.viewWithTag(1000) as! UILabel
